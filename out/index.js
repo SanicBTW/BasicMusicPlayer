7e1f99967aa0c24d0a1839eebffa639fbd249474
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2022-06-03 20:36:10 +0000

    i tried with typescript

--- out/index.js
+++ out/index.js
@@ -1,15 +1,37 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-define(["require", "exports", "./docElements", "./helperVariables", "./fileSysOperations"], function (require, exports, docElements_1, helperVariables_1, fileSysOperations_1) {
+define(["require", "exports", "./docElements", "./fileSysOperations"], function (require, exports, docElements_1, fileSysOperations_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     docElements_1 = __importDefault(docElements_1);
-    helperVariables_1 = __importDefault(helperVariables_1);
+    //type definitions my beloved
+    //simplified index? yessir
+    //var daMusic:Array<string[][]> = [[],[]];
+    var musicArray = []; //gotta check what type is the thing
+    var musicNameArray = [];
+    var musicPath = './music/'; //old support???
+    var dataExt = ".json";
+    var listMusicFile = "listMusic.txt";
+    var curIdx = 0; //might use a custom index in the index file instead of this one, idk
+    var repeatMusic = false;
+    var musicPlaying = false;
+    var musicPaused = false;
+    var doneSearchingFiles = false;
+    var changedMusicPath = false;
     docElements_1.default.audioElement.onended = function () {
-        helperVariables_1.default.musicPlaying = false;
-        helperVariables_1.default.musicPaused = false;
-        if (helperVariables_1.default.repeatMusic) //if its true
+        musicPlaying = false;
+        musicPaused = false;
+        if (repeatMusic) //if its true
          {
             //setState("");
         }
@@ -18,32 +40,71 @@ define(["require", "exports", "./docElements", "./helperVariables", "./fileSysOp
         }
     };
     docElements_1.default.audioElement.onplaying = function () {
-        helperVariables_1.default.musicPlaying = true;
-        helperVariables_1.default.musicPaused = false;
+        musicPlaying = true;
+        musicPaused = false;
         docElements_1.default.playButton.innerText = "Pause";
     };
     docElements_1.default.audioElement.onpause = function () {
-        helperVariables_1.default.musicPlaying = false;
-        helperVariables_1.default.musicPaused = false;
+        musicPlaying = false;
+        musicPaused = false;
         docElements_1.default.playButton.innerText = "Resume";
-        docElements_1.default.curPlayingInfo.innerText = "Currently playing: " + helperVariables_1.default.musicNameArray[helperVariables_1.default.curIdx] + " (PAUSED)";
+        docElements_1.default.curPlayingInfo.innerText = "Currently playing: " + musicNameArray[curIdx] + " (PAUSED)";
     };
     //add the duration indicator and volume maybe
-    (0, fileSysOperations_1.setupFiles)();
-    alert(helperVariables_1.default.musicArray);
-    alert(helperVariables_1.default.musicNameArray);
+    setupFiles();
     function setPlayerState(state) {
         switch (state) {
             case "play":
-                docElements_1.default.audioElement.src = helperVariables_1.default.musicPath[helperVariables_1.default.curIdx];
+                docElements_1.default.audioElement.src = musicArray[curIdx];
                 docElements_1.default.audioElement.load();
                 docElements_1.default.audioElement.play();
-                docElements_1.default.curPlayingInfo.innerText = "Currently playing: " + helperVariables_1.default.musicNameArray[helperVariables_1.default.curIdx];
+                docElements_1.default.curPlayingInfo.innerText = "Currently playing: " + musicNameArray[curIdx];
                 break;
         }
     }
-    docElements_1.default.audioElement.src = helperVariables_1.default.musicPath[helperVariables_1.default.curIdx];
+    docElements_1.default.audioElement.src = musicArray[curIdx];
     docElements_1.default.audioElement.load();
     docElements_1.default.audioElement.play();
-    docElements_1.default.curPlayingInfo.innerText = "Currently playing: " + helperVariables_1.default.musicNameArray[helperVariables_1.default.curIdx];
+    docElements_1.default.curPlayingInfo.innerText = "Currently playing: " + musicNameArray[curIdx];
+    function setupFiles() {
+        return __awaiter(this, void 0, void 0, function* () {
+            cleanArrays();
+            var details = [];
+            if (doneSearchingFiles == false) {
+                try {
+                    details = yield (0, fileSysOperations_1.readFile)(musicPath + listMusicFile, true);
+                }
+                catch (exception) {
+                    alert("oops code fucked up gotta try with default path man");
+                    musicPath = './music/'; //old support??
+                    yield setupFiles();
+                }
+                for (var i in details) {
+                    //music path should come formatted already
+                    var advancedDetails = details[i].split(":");
+                    /* info about this new thing
+                    * advancedDetails[0] should be the fileName including .mp3
+                    * advancedDetails[1] should be the name to be displayed
+                    */
+                    var musicDir = musicPath + advancedDetails[0];
+                    musicArray.push(musicDir);
+                    alert(musicDir);
+                    alert(musicArray[0]);
+                    musicNameArray.push(advancedDetails[1]);
+                    docElements_1.default.songList.innerText = docElements_1.default.songList.innerText + "\n" + musicNameArray[i];
+                }
+                doneSearchingFiles = true;
+            }
+        });
+    }
+    function cleanArrays() {
+        docElements_1.default.songList.innerText = "Source: " + musicPath;
+        docElements_1.default.songListLabel.innerText = "Available music: ";
+        if (musicArray.length > 0) {
+            musicArray = [];
+        }
+        if (musicNameArray.length > 0) {
+            musicNameArray = [];
+        }
+    }
 });
--- src/fileSysOperations.ts
+++ src/fileSysOperations.ts
@@ -1,8 +1,3 @@
-import { cleanArrays } from "./helperVariables";
-import editableVars from './helperVariables';
-import docElements from "./docElements";
-
-
 export async function readFile(file:string, turnIntoArray:boolean = false):Promise<any> { //lets just change it to any, kind of lazy to deal with type shit lol
     var allText:string = "";
     var raw:Response = await fetch(file);
@@ -21,6 +16,7 @@ export async function readFile(file:string, turnIntoArray:boolean = false):Promi
     return allText;
 }
 
+//not used anymore, ig
 export async function readFileJSON(file:string):Promise<any> {
     var allText:any;
     var raw:Response = await fetch(file);
@@ -33,37 +29,4 @@ export async function readFileJSON(file:string):Promise<any> {
         alert(exp)
     }
     return allText;
-}
-
-export async function setupFiles(){
-    cleanArrays();
-
-    if(editableVars.doneSearchingFiles == false)
-    {
-        var listMusic:Array<string> = [];
-        try
-        {
-            listMusic = await readFile(editableVars.musicPath + editableVars.listMusicFile, true);
-        } 
-        catch (exception) 
-        {
-            alert("oops code fucked up gotta try with default path man");
-            editableVars.musicPath = './music/' //old support??
-            await setupFiles();
-        }
-        for(var i in listMusic)
-        {
-            //music path should come formatted already
-            var mainDir = editableVars.musicPath + listMusic[i] + "/"; //format it just in case
-            var dataDir = mainDir + "data" + editableVars.dataExt; //yo i just had an idea, what if we just specify the details in the listMusic file (song:songname:filename) it should be epic
-            var theJSON = await readFileJSON(dataDir);
-            alert(theJSON)
-            var musicDir = mainDir + theJSON['fileName'];
-            alert(musicDir);
-            editableVars.musicArray.push(musicDir);
-            editableVars.musicNameArray.push(theJSON['name']);
-            docElements.songList!.innerText = docElements.songList!.innerText + "\n" + editableVars.musicNameArray[i];
-        }
-        editableVars.doneSearchingFiles = true;
-    }
 }
