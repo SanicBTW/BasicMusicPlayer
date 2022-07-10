d23f92deb1b2857bf718671cad7c412eac2772b4
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2022-07-10 10:01:12 +0000

    prepare for user settings :sunglasses:

--- source/progressBar.js
+++ source/progressBar.js
@@ -2,8 +2,10 @@ elements.addDiv("myProgress");
 elements.addDiv("myBar");
 elements.appendTo("myBar", "myProgress");
 
-styles.setStyle("myProgress", "width: 50%; background-color:white;");
-styles.setStyle("myBar", "width: 0%; height: 20px; background-color: aqua;");
+//theres a visual bug where at the beginning of the song the bar doesnt display correctly
+//border-radius: 0px 12px 12px 0px;
+styles.setStyle("myProgress", "width: 100%; background-color:white; border: none; ");
+styles.setStyle("myBar", "width: 0%; height: 20px; background-color: aqua; border: none; ");
 
 var daProg = elements.getElement("myProgress");
 var daBar = elements.getElement("myBar");
