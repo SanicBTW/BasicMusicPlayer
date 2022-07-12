0a1d5f08f63979596ac53df2fb50d9c5a93d7b83
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2022-07-12 01:24:34 +0000

    first setting available on the settings panel, will make it better soon

--- source/settingsPanel.js
+++ source/settingsPanel.js
@@ -1,6 +1,10 @@
 //totally not the old settings page
 var daSettingsPanel = document.getElementById("settingsSidePanel");
 
+var daProg = document.getElementById("timeProgress");
+var daBar = document.getElementById("timeBar");
+var daBackColorInput = document.getElementById("colorInput1");
+
 function openSettingsPanel()
 {
     daSettingsPanel.style.width = "100%";
@@ -9,4 +13,18 @@ function openSettingsPanel()
 function closeSettingsPanel()
 {
     daSettingsPanel.style.width = "0%";
+}
+
+function applySettings(setting)
+{
+    switch(setting)
+    {
+        case "time bar background color":
+            const color = daBackColorInput.value;
+            const r = parseInt(color.substr(1,2), 16);
+            const g = parseInt(color.substr(3,2), 16);
+            const b = parseInt(color.substr(5,2), 16);
+            daProg.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+            break;
+    }
 }
