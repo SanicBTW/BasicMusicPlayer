af065d069b563de9bc19356b888799dc1643bf1e
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2022-07-12 00:53:59 +0000

    rewrite done, lookin real good

---
+++ source/settingsPanel.js
@@ -0,0 +1,12 @@
+//totally not the old settings page
+var daSettingsPanel = document.getElementById("settingsSidePanel");
+
+function openSettingsPanel()
+{
+    daSettingsPanel.style.width = "100%";
+}
+
+function closeSettingsPanel()
+{
+    daSettingsPanel.style.width = "0%";
+}
