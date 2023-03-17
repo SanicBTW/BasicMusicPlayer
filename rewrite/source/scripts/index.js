fb6b885294e5cae2750bcf3a52710db097ea0e3a
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2023-03-17 18:16:13 +0100

    Lerps everywhere

--- rewrite/source/scripts/index.js
+++ rewrite/source/scripts/index.js
@@ -1,9 +1,67 @@
+// Funny vars
+const fpsLoop = new FramerateLoop();
 const content = document.getElementById('mainContent');
 const audio = document.getElementById('player');
 const headText = document.getElementById('headText');
 
+// Volume tray vars
+const volTray = document.getElementById('volumeTray');
+const volBar = document.getElementById('volBar');
+
+var visibleTime = 0;
+var targetPos = -200;
+
+// Time info vars
+const timeBar = document.getElementById('timeBar');
+
+// Supported audio types )?
 const supportedTypes = [".mp3", ".ogg"];
 
+// Volume stuff
+const volumeValues = 
+{
+    1: "100%",
+    0.9: "90%",
+    0.8: "80%",
+    0.7: "70%",
+    0.6: "60%",
+    0.5: "50%",
+    0.4: "40%",
+    0.3: "30%",
+    0.2: "20%",
+    0.1: "10%",
+    0: "0%"
+};
+
+function getVolume()
+{
+    switch(audio.volume)
+    {
+        case 1:
+            return 1;
+        case 0.9:
+            return 0.9;
+        case 0.8:
+            return 0.8;
+        case 0.7000000000000001:
+            return 0.7;
+        case 0.6000000000000001:
+            return 0.6;
+        case 0.5000000000000001:
+            return 0.5;
+        case 0.40000000000000013:
+            return 0.4;
+        case 0.30000000000000016:
+            return 0.3;
+        case 0.20000000000000015:
+            return 0.2;
+        case 0.10000000000000014:
+            return 0.1;
+    }
+    return 0;
+}
+
+// Handles drop
 function dropHandler(e)
 {
     e.preventDefault();
@@ -29,6 +87,7 @@ function dropHandler(e)
     }
 }
 
+// Handles file
 function handleFile(file)
 {
     var fileExt = file.name.substring(file.name.indexOf("."));
@@ -41,24 +100,51 @@ function handleFile(file)
         audio.src = URL.createObjectURL(blob);
         content.style.opacity = 1;
         headText.innerText = fileName;
-        document.getElementById('timeBar').style.backgroundColor = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
+        timeBar.style.backgroundColor = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
     });
 }
 
+// Set audio listener
+audio.addEventListener('durationchange', () => timeBar.style.width = "0%");
+
+// Set the FPS Loop update
+fpsLoop.customUpdate = () =>
+{
+    var lerpVal = boundTo(1 - (fpsLoop.elapsed * 8.6), 0, 1);
+    // Volume tray Y position
+    var volTY = volTray.style.top.substring(0, volTray.style.top.indexOf("px"));
+    // Number of the volume bar width
+    var volTar = volBar.style.width.substring(0, volBar.style.width.indexOf("%"));
+    // Number of the time bar width
+    var timeTar = timeBar.style.width.substring(0, timeBar.style.width.indexOf("%"));
+
+    // Funny tray position
+    if (visibleTime > 0)
+    {
+        visibleTime -= fpsLoop.elapsed;
+    }
+    else if (volTY > -200)
+    {
+        targetPos -= fpsLoop.elapsed * window.visualViewport.height * (window.visualViewport.width / window.visualViewport.height);
+    }
+
+    // Lerps
+    volTray.style.top = `${lerp(targetPos, volTY, lerpVal)}px`;
+    volBar.style.width = `${lerp(audio.volume * 100, volTar, lerpVal)}%`;
+    timeBar.style.width = `${lerp((audio.currentTime / audio.duration) * 100, timeTar, lerpVal)}%`;
+
+    // Set info
+    document.getElementById("volP").innerText = `${Math.round(volTar)}%`;
+    document.getElementById('timeDisplay').innerText = `${getCurrentTime()}/${getDuration()}`;
+}
+
+// Helper functions
 function getRandomColor()
 {
     return Math.floor(Math.random() * 255);
 }
 
-function setProgress(amount)
-{
-    document.getElementById("timeBar").style.width = amount + "%";
-}
-
-audio.addEventListener('durationchange', () => setProgress(0));
-audio.addEventListener('timeupdate', () => updateTime());
-
-function updateTime()
+function getDuration()
 {
     var lengthMin = Math.floor(audio.duration / 60);
     if(lengthMin >= 10)
@@ -96,48 +182,46 @@ function updateTime()
         }
     }
 
-    var songPercent = (audio.currentTime / audio.duration);
+    return `${lengthMin}:${lengthSecs}`;
+}
+
+function getCurrentTime()
+{
     var curMin = Math.floor(audio.currentTime / 60) >= 10 ? Math.floor(audio.currentTime / 60) : "0" + Math.floor(audio.currentTime / 60);
     var curSecs = Math.floor(audio.currentTime % 60) >= 10 ? Math.floor(audio.currentTime % 60) : "0" + Math.floor(audio.currentTime % 60);
 
-    var songCalc = (audio.duration - audio.currentTime);
-    var minLeft = Math.floor(songCalc / 60);
-    if(minLeft >= 10)
-    {
-        minLeft = Math.floor(songCalc / 60);
-    }
-    else
-    {
-        minLeft = "0" + Math.floor(songCalc / 60);
-        if(minLeft == "0NaN")
-        {
-            minLeft = "00";
-        }
-        else
-        {
-            minLeft = "0" + Math.floor(songCalc / 60);
-        }
-    }
-
-    var secsLeft = Math.floor(songCalc % 60);
-    if(secsLeft >= 10)
-    {
-        secsLeft = Math.floor(songCalc % 60);
-    }
-    else
-    {
-        secsLeft = "0" + Math.floor(songCalc % 60);
-        if(secsLeft == "0NaN")
-        {
-            secsLeft = "00";
-        }
-        else
-        {
-            secsLeft = "0" + Math.floor(songCalc % 60);
-        }
-    }
-
-    document.getElementById('timeDisplay').innerText = `${curMin}:${curSecs}/${minLeft}:${secsLeft}`;
-
-    setProgress(songPercent * 100);
+    return `${curMin}:${curSecs}`;
 }
+
+function boundTo(value, min, max)
+{
+    return Math.max(min, Math.min(max, value));
+}
+
+function lerp(a, b, ratio)
+{
+    return a + ratio * (b - a);
+}
+
+// More listeners
+document.addEventListener('keydown', (key) =>
+{
+    if (key.key == "+" || key.key == "-")
+    {
+        key.preventDefault();
+        visibleTime = 1.5;
+        targetPos = 0;
+
+        switch(key.key)
+        {
+            case "+":
+                if (getVolume() != 1.0)
+                    audio.volume += 0.1;
+                break;
+            case "-":
+                if (getVolume() != 0.0)
+                    audio.volume -= 0.1;
+                break;
+        }
+    }
+});
